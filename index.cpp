38a11769b3e0263f2734672480a5d1954b8adff4
RJay-Pickering <rjaypickering@gmail.com>
2023-03-03 15:33:56 -0600

    made a simple bookstore in C++, on monday, I plan on starting on the casino_guessing_game file

--- index.cpp
+++ index.cpp
@@ -24,48 +24,22 @@ void win ()
 
 int main() 
 {   
-label:
-std::cout << "Enemy Health: " << ehp << std::endl << "Your Health: " << hp << std::endl << "Mana: " << mp << std::endl << "(1) Attack" << std::endl << "(2) Magic Missile" << std::endl << "(3) Summon" << std::endl; 
-std::cin >> move;
-std::cout << "Status:" << std::endl;
+	label:
+	std::cout << "Enemy Health: " << ehp << std::endl << "Your Health: " << hp << std::endl << "Mana: " << mp << std::endl << "(1) Attack" << std::endl << "(2) Magic Missile" << std::endl << "(3) Summon" << std::endl; 
+	std::cin >> move;
+	std::cout << "Status:" << std::endl;
 
-if (move == 1) 
-{
-	std::cout << "You sliced the enemy with your sword." << std::endl; 
-	ehp -= 1;  
-	if (ehp <= 0) 
-	{ 
-		win();  
-		return 0;
-	} 
-
-	std::cout << "The enemy uses his sword." << std::endl << std::endl; 
-	hp -= 1; 
-
-	if (hp <= 0) 
-	{ 
-		lose();  
-		return 0;
-	}
-} 
-
-if (move == 2) 
-{ 
-	if (mp < 1)
+	if (move == 1) 
 	{
-		std::cout << "You don`t have enough Mana" << std::endl;
-	} else
-	{
-		std::cout << "You cast a devastating Magic Missile." << std::endl; 
-		ehp -= 2; 
-		mp -= 1; 
+		std::cout << "You sliced the enemy with your sword." << std::endl; 
+		ehp -= 1;  
 		if (ehp <= 0) 
 		{ 
 			win();  
 			return 0;
 		} 
 
-		std::cout << "The enemy used his sword." << std::endl << std::endl; 
+		std::cout << "The enemy uses his sword." << std::endl << std::endl; 
 		hp -= 1; 
 
 		if (hp <= 0) 
@@ -73,40 +47,66 @@ if (move == 2)
 			lose();  
 			return 0;
 		}
-	}
-}   
+	} 
 
-if (move == 3) 
-{ 
-	if (mp < 15)
-	{
-		std::cout << "You don`t have enough Mana" << std::endl;
-	} else
-	{
-		std::cout << "You summoned Ultima!" << std::endl; 
-		ehp -= 10; 
-		mp -= 15;  
-		hp -= 1;
-		if (ehp <= 0) 
-		{ 
-			win();  
-			return 0;
-		} 
+	if (move == 2) 
+	{ 
+		if (mp < 1)
+		{
+			std::cout << "You don`t have enough Mana" << std::endl;
+		} else
+		{
+			std::cout << "You cast a devastating Magic Missile." << std::endl; 
+			ehp -= 2; 
+			mp -= 1; 
+			if (ehp <= 0) 
+			{ 
+				win();  
+				return 0;
+			} 
 
-		std::cout << "The enemy used his sword." << std::endl << std::endl; 
-		hp -= 1;
+			std::cout << "The enemy used his sword." << std::endl << std::endl; 
+			hp -= 1; 
 
-		if (hp <= 0) 
-		{ 
-			lose();  
-			return 0;
+			if (hp <= 0) 
+			{ 
+				lose();  
+				return 0;
+			}
+		}
+	}   
+
+	if (move == 3) 
+	{ 
+		if (mp < 15)
+		{
+			std::cout << "You don`t have enough Mana" << std::endl;
+		} else
+		{
+			std::cout << "You summoned Ultima!" << std::endl; 
+			ehp -= 10; 
+			mp -= 15;  
+			hp -= 1;
+			if (ehp <= 0) 
+			{ 
+				win();  
+				return 0;
+			} 
+
+			std::cout << "The enemy used his sword." << std::endl << std::endl; 
+			hp -= 1;
+
+			if (hp <= 0) 
+			{ 
+				lose();  
+				return 0;
+			}
 		}
 	}
-}
 
-if (mp <= 0) {
-	mp = 0;
-}
+	if (mp <= 0) {
+		mp = 0;
+	}
 
-goto label;
+	goto label;
 }
